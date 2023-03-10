89026b6bdfcd0406d36ac91a02f3ca96ac4a16e8
Qaiser Abbas <mqaiser617@gmail.com>
2023-03-10 14:48:29 +0500

    Update youtube_videos_capture_frames.py

--- youtube_videos_capture_frames.py
+++ youtube_videos_capture_frames.py
@@ -3,28 +3,34 @@
 import cv2
 import os
 from vidgear.gears import CamGear
-### Replace this youtube video link with your one
-#https://www.youtube.com/watch?v=lQc432kUOpM
-stream = CamGear(source='https://www.youtube.com/watch?v=69X8ZLj1ukw', stream_mode =True,  time_delay=1, logging=True).start()
 
-#this should be the path where u want ur images
+### Replace this youtube video link with your one
+# https://www.youtube.com/watch?v=lQc432kUOpM
+stream = CamGear(
+    source="https://www.youtube.com/watch?v=69X8ZLj1ukw",
+    stream_mode=True,
+    time_delay=1,
+    logging=True,
+).start()
+
+# this should be the path where u want ur images
 # path  = 'C:\\Users\\user\\Desktop\\Capture Frames from Online YouTube Video\\cars\\'
 path = "D:\\github codes\\test\\"
 
 currentframe = 0
 while True:
 
-    frame = stream.read() ### using functions from vidGear module
+    frame = stream.read()  ### using functions from vidGear module
     if frame is None:
         break
 
-    cv2.imshow("Output Frame", frame) # optional if u want to show the frames
+    cv2.imshow("Output Frame", frame)  # optional if u want to show the frames
 
-    name = path + './frames' + str(currentframe) + '.jpg'
-    print ('Creating...' + name) 
+    name = path + "./frames" + str(currentframe) + ".jpg"
+    print("Creating..." + name)
 
     cv2.imwrite(name, frame)
-    currentframe += 5 ##chnage 5 with the number of frames. Here 5 means capture frame after every 5 frames
+    currentframe += 5  ##chnage 5 with the number of frames. Here 5 means capture frame after every 5 frames
     ###usually videos are 30fps so if here 30 is provided a frame will be captures after every second.
 
     key = cv2.waitKey(1) & 0xFF
